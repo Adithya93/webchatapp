86a461d4a26dc57f3d7c91d3df02dd8e38bb924c
Adithya <ra102@duke.edu>
2016-07-08 22:09:20 -0700

    Procfile and mention of start script in package.json for heroku deploy

--- index.js
+++ index.js
@@ -108,7 +108,7 @@ io.on('connection', function(socket) {
     });
 });
 
-http.listen(3000, function() {
+http.listen(process.env.PORT || 3000, function() {
     console.log("Listening on port 3000");
 });
 
